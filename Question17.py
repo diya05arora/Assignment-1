3ed026eba51d05e5577effa5732069212ea5c35c
Diya Arora <diya05arora@gmail.com>
2024-06-12 20:01:03 +0530

    Question 17

---
+++ Question17.py
@@ -0,0 +1,3 @@
+# Write a program in python that converts a given string to title case (first letter of each word capitalized).
+str = input("Enter a string: ")
+print("The given string", str, "in title case is:", str.title())
