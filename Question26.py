4b47c1e5fbbc984822b597c9e814924280fea586
Diya Arora <diya05arora@gmail.com>
2024-06-12 23:30:21 +0530

    Question 27

--- Question26.py
+++ Question26.py
@@ -1,6 +1,6 @@
 # Write a program in python that checks if a string starts with a given prefix or ends with a given suffix
 string = input("Enter a string: ")
-choice = int(input("Enter 1 to check if the string starts with given prefix\n]\
+choice = int(input("Enter 1 to check if the string starts with given prefix\n\
 Enter 2 to check if the string ends with given suffix\n"))
 if(choice==1):
     prefix=input("Enter a prefix to check if the string starts with it or not: ")
---
+++ Question27.py
@@ -0,0 +1,4 @@
+# Write a program in python that converts a string into a list of its characters
+string = input("Enter a string to convert it into list of characters: ")
+list1 = list(string)
+print("The list of characters of string", string, "is", list1)
