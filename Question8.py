a0e4dfab6f456ece3dc1170bac911d4657c38676
Diya Arora <diya05arora@gmail.com>
2024-06-12 19:19:16 +0530

    Question 8

---
+++ Question8.py
@@ -0,0 +1,5 @@
+# Write a python program that concatenates two strings and returns the result.
+str1 = input("Enter the first string: ")
+str2 = input("Enter the sceond string: ")
+concatenatedStr = str1 + str2
+print("The concatenated string is:", concatenatedStr)
