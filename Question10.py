bde0b096a93e43f7fdf82caf2334667ff631ea3d
Diya Arora <diya05arora@gmail.com>
2024-06-12 19:25:11 +0530

    Question 10

---
+++ Question10.py
@@ -0,0 +1,3 @@
+# Write a python program that converts a given string to uppercase.
+str = input("Enter a string to convert it into uppercase: ")
+print("The string", str, "in uppercase is", str.upper())
