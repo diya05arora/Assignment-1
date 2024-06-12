db2417eb8a15fa03bc62fdbc47e527ac2070c79b
Diya Arora <diya05arora@gmail.com>
2024-06-12 19:13:19 +0530

    Question 6

---
+++ Question6.py
@@ -0,0 +1,4 @@
+# Write a program that reads the content of a text file and prints it to the console.
+fileObj = open("C:/Users/diya0/Machine Learning/Assignment-1/fileQ6.txt")
+text = fileObj.read()
+print("The content of file is:\n", text)
