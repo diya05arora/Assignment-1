9d6accbae7184c717f412a32d664d095ff1b2bb7
Diya Arora <diya05arora@gmail.com>
2024-06-17 14:59:07 +0530

    Question 3

--- Question3.py
+++ Question3.py
@@ -1,6 +1,9 @@
 # Write a python program that calculates the factorial of a given number.
 num = int(input("Enter a number to get its factorial: "))
-fact = 1
+fact=1
+if(num<0):
+    print("Cannot calculate the factorrial of negative number")
+    fact="Infinity"
 for i in range(num, 0, -1):
     fact *= i
 print("The factorial of", num, "=", fact)
